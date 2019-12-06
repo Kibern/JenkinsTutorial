5a63fbbfe5f7d5da5dc7f3b4e80e33f7f738c2fc
Kuttubek <a312tec@Kuttubeks-MacBook-Pro.local>
2019-12-05 20:22:42 -0500

    dsa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	// write your code here
         for(int i =0;i< 20;i++){
             System.out.println(i+"Iterated");
+            ///qweqweqwewqewqeqwqeweqw push
         }
     }
 }
